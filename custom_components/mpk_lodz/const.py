b34d65924b200de731b3d810476f84bf8b0d9b87
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-11-11 01:28:08 +0100

    Create const.py

---
+++ custom_components/mpk_lodz/const.py
@@ -0,0 +1,18 @@
+"""Constants for the MPK Łódź integration."""
+from typing import Final
+
+DOMAIN: Final = "mpk_lodz"
+DEFAULT_NAME: Final = "MPK Łódź"
+
+CONF_ID = "id"
+CONF_NAME = "name"
+CONF_NUM = "num"
+CONF_GROUP = 'group'
+CONF_STOPS = "stops"
+CONF_LINES = "lines"
+CONF_DIRECTIONS = "directions"
+CONF_STOP_TYPE = "stop_type"
+
+STOP_TYPE_ID = "id"
+STOP_TYPE_NUM = "num"
+STOP_TYPE_GROUP = "group"
